fa7405256754a44541cb5b358c76d6b9098b68c2
unknown <williampo78@gmail.com>
2021-09-03 11:29:08 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div v-for="(card, index) in cards" :key="index" class="card">
+      {{ card.word }}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
+import axios from "axios";
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
+  },
+  data() {
+    return {
+      cards: [{ word: "", partOfSpeech: "", translation: "", example: "" }],
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:3000/cards").then((res) => {
+      console.log(res.data);
+      this.cards.push(this.cards.push(res.data));
+    });
   },
 };
 </script>
