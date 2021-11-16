6ee3f75b16945c83bf0acc1ae8d26d36b1e70323
unknown <williampo78@gmail.com>
2021-11-16 17:46:06 +0800

    blur when delete check

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,11 @@
       <h1>單字列表</h1>
       <div class="cardsBox">
         <ol>
-          <li v-for="(card, index) in cards" :key="index">
+          <li
+            :class="{ blur: deleteCard == index && deleteCheck }"
+            v-for="(card, index) in cards"
+            :key="index"
+          >
             <div v-if="deleteCard == index && deleteCheck" class="dlt-check">
               <i @click="deleteCheck = false" class="fas fa-times-circle"></i>
               <p>確定要刪除嗎</p>
@@ -16,6 +20,7 @@
                 鼻要
               </button>
             </div>
+
             <p v-if="editedCard !== index" class="word">
               {{ card.word }} <span class="pos">({{ card.partOfSpeech }})</span>
             </p>
@@ -158,16 +163,18 @@ export default {
   },
   methods: {
     startEdit(index) {
-      this.editInput.word = this.cards[index].word;
-      this.editInput.partOfSpeech = this.cards[index].partOfSpeech;
-      this.editInput.translation = this.cards[index].translation;
-      this.editInput.example = this.cards[index].example;
-      this.editedCard = index;
-      console.log(this.editedCard, index);
+      if (!this.deleteCheck) {
+        this.editInput.word = this.cards[index].word;
+        this.editInput.partOfSpeech = this.cards[index].partOfSpeech;
+        this.editInput.translation = this.cards[index].translation;
+        this.editInput.example = this.cards[index].example;
+        this.editedCard = index;
+        console.log(this.editedCard, index);
+      }
     },
     deleteModal(index) {
       this.deleteCard = index;
-      this.deleteCheck = !this.deleteCheck;
+      this.deleteCheck = true;
     },
     deleteHandler(index) {
       // let target = this.cards[index];
@@ -239,6 +246,7 @@ export default {
           display: flex;
           flex-direction: column;
           align-items: center;
+
           .dlt-check {
             position: absolute;
             text-align: center;
@@ -248,6 +256,7 @@ export default {
             background: rgb(255, 255, 255);
             border-radius: 5px;
             box-shadow: 0 3px 7px 0 rgb(0 0 0 / 32%);
+            z-index: 5;
             .fa-times-circle {
               position: absolute;
               right: 8px;
@@ -262,6 +271,7 @@ export default {
               font-weight: bold;
               text-align: center;
               color: rgb(0, 0, 0);
+              filter: none;
             }
             .dlt {
               width: 35%;
@@ -291,7 +301,6 @@ export default {
             }
           }
           p {
-            // display: none;
             width: 90%;
             height: auto;
             margin: 10px 0;
@@ -322,7 +331,7 @@ export default {
             position: absolute;
             top: 10px;
             right: 35px;
-            transition: all 0.5s ease;
+            // transition: all 0.5s ease;
             color: rgb(39, 132, 238);
           }
           .fa-trash-alt {
@@ -330,7 +339,7 @@ export default {
             top: 10px;
             right: 10px;
             color: rgb(209, 39, 39);
-            transition: all 0.5s ease;
+            // transition: all 0.5s ease;
           }
           .edit {
             width: 100%;
@@ -416,6 +425,19 @@ export default {
             .cancel {
               background: rgba(17, 48, 100, 0.61);
             }
+          }
+        }
+        .blur {
+          p {
+            filter: blur(2px);
+          }
+          .fa-trash-alt {
+            filter: blur(2px);
+            cursor: default;
+          }
+          .fa-edit {
+            filter: blur(2px);
+            cursor: default;
           }
         }
       }
